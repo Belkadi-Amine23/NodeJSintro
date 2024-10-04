11dc9f46d5da8a31c387b20abee70609f451bcf8
Amine Belkadi <m.aminebelkadi@gmail.com>
2024-10-04 20:15:05 +0100

    feat(express): adds express.js
    n

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 /*import fs from "fs"
 const fileContent = fs.readFileSync('./package.json')
 console.log(fileContent.toString())*/
-import http from "http"
+
+
+/*import http from "http"
 
 const server =http.createServer((req, res)=> {
     res.writeHead(200, { "Content-Type": "text/plain"})
@@ -12,4 +14,33 @@ server.listen(3000, () => {
     console.log("Server is running on port 3000")
 })
 
-// AMINE BELKADI
+// AMINE BELKADI*/
+
+import express from "express"
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.status(200).send("Hello World!")
+});
+
+
+app.get("/about", (req, res) => {
+    res.status(200).send("About Us")
+});
+
+app.get("/page1.html", (req, res) => {
+    res.status(200).send("Page 1" + req.socket.localAddress + req.socket.remoteFamily)
+});
+
+app.post("/contact", (req, res) => {
+    res.status(200).send("Contact Us")
+});
+
+app.use(express.static("public"))
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000")
+})
+
+// AMINE BELKADI*
